fbfc6b0c9233d1ff6b099a33dc0903a08652c1e7
pastelcyborg <168636120+pastelcyborg@users.noreply.github.com>
2024-06-06 17:32:31 +0100

    Add Google gapi for Sheets integration (#4)
    * Add gapi and initial sheets call
    
    * Begin parsing sheet data
    
    * Render sheet data to table
    
    * Use virtual table body

--- ts/main.ts
+++ ts/main.ts
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 function setupAppLayoutExamples() {
   var aside = document.querySelector(".l-aside");
   var navigation = document.querySelector(".l-navigation");
@@ -77,3 +79,67 @@ function setupAppLayoutExamples() {
 }
 
 setupAppLayoutExamples();
+
+function constructTableRow(rowData) {
+  const tableRowEl = document.createElement("tr");
+
+  rowData.forEach((cellData) => {
+    const tableCellEl = document.createElement("td");
+
+    tableCellEl.textContent = cellData;
+
+    tableRowEl.appendChild(tableCellEl);
+  });
+
+  return tableRowEl;
+}
+
+function renderSheetData(sheetValues) {
+  console.log(sheetValues);
+
+  const vanillaUsageStatsTable = document.getElementById(
+    "vanilla-usage-stats-table"
+  );
+  const virtualTableBodyEl = document.createDocumentFragment();
+
+  sheetValues.forEach((rowData, index) => {
+    // Hack because the Sheets data currently has headers in the first row
+    if (index !== 0) {
+      const tableRowEl = constructTableRow(rowData);
+
+      virtualTableBodyEl.appendChild(tableRowEl);
+    }
+  });
+
+  vanillaUsageStatsTable.appendChild(virtualTableBodyEl);
+}
+
+async function retrieveSheetData() {
+  try {
+    await gapi.client.init({
+      apiKey: "AIzaSyDdu12mIqCIryiiwgpDshVIRg-ZiBOWl_I",
+      discoveryDocs: [
+        "https://sheets.googleapis.com/$discovery/rest?version=v4",
+      ],
+    });
+  } catch (error) {
+    throw new Error("Error initializing gapi client - check your API key!");
+  }
+
+  let sheetValues;
+
+  try {
+    const sheetResponse = await gapi.client.sheets.spreadsheets.values.get({
+      spreadsheetId: "1vL8Z7BhmcLxkNGKQm6emI0gQwy_e4Lvq5PUqdCEw7C4",
+      range: "Sheet1",
+    });
+
+    sheetValues = sheetResponse.result.values;
+  } catch (err) {
+    throw new Error("Error getting sheet values - check your spreadsheet ID!");
+  }
+
+  renderSheetData(sheetValues);
+}
+
+gapi.load("client", retrieveSheetData);
